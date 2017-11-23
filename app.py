3a34bf4bd2c5363bc1ffc2b7f66a0e6e9d8c5964
meshnesh <tonnie.nesh@gmail.com>
2017-11-23 04:34:49 +0300

    update files to python 3

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import unittest
 
 class ContinousTesting(unittest.TestCase):
     def setUp(self):
-        print "some stuff"
+        print ("some stuff")
 
     def test_search_in_python(self):
-        print "some text"
+        print ("some text")
 
 if __name__ == "__main__":
     unittest.main
